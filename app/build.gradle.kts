13e38e1d071ca13cf8c4a498f07f32ec62258913
icepony <44568304+icepony@users.noreply.github.com>
2026-09-24 10:28:02 +0800

    tag v2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.icepony.alwaysbatterysaver"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
     }
 
     buildFeatures {
